ae5c94658588d46ea6a130518c4b94cebab06ae4
simpx <simpxx@gmail.com>
2025-12-23 13:16:08 +0800

    fix

--- packice/__init__.py
+++ packice/__init__.py
@@ -1,5 +1,6 @@
 """PackIce packaging."""
 
 from .interface.client import Client, connect, Object
+from .peers.memory import MemoryPeer
 
 __all__ = ["Client", "connect", "Object", "MemoryPeer", "backends", "transport", "peers"]
